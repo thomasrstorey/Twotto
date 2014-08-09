7d8dd5d4dc2af9c0c134d511b94778cf54cae77c
Thomas Storey <storey.thomas@gmail.com>
2014-08-08 22:36:57 -0400

    loading bars, disable buttons during ajax requests

--- public/js/controllers/UserCtrl.js
+++ public/js/controllers/UserCtrl.js
@@ -1,29 +1,34 @@
 angular.module('UserCtrl', []).controller('UserController', ['$scope', '$rootScope', '$http',
 	function ($scope, $rootScope, $http) {
 		$scope.user = {};
+		$scope.loading = false;
 
 		function loadUser() {
 			console.log("Hey get a user or whatver");
 			$http.get('/app/secured/id')
 			.success(function(data){
-				$scope.userid = data.message || null;
+				$scope.message = data.message || null;
 				$scope.user = data.user || null;
 			});
 		};
 
 		$scope.fillDB = function () {
 			console.log("Fill the database!");
+			$scope.loading = true;
 			$http.post('/app/secured/db/' + $scope.user.twitter.username)
 			.success(function(data){
-				$scope.userid = data.message || null;
+				$scope.loading = false;
+				$scope.message = data.message || null;
 			});
 		};
 
 		$scope.tweet = function () {
 			console.log("tweet tweet");
+			$scope.loading = true;
 			$http.post('app/secured/tweet/' + $scope.user.twitter.username)
 			.success(function(data){
-				$scope.userid = data.message || null;
+				$scope.loading = false;
+				$scope.message = data.message || null;
 			});
 		};
 
